27524a40c32e0ff6bb809a37261ef62920cfb399
Kleyton João <kleyton.joao@gmail.com>
2019-06-04 00:13:22 -0300

    configurando para mostra onde esta salvando imagem

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -12,7 +12,8 @@ router.get('/', async(req, res)=>{
 
 router.post('/', async(req, res)=>{
     const {title, author, isbn} = req.body
-    const newBook = new Book({title, author, isbn})
+    const image = '/uploads/' + req.file.filename;
+    const newBook = new Book({title, author, isbn, image})
     newBook.save();
     res.json({message: 'Book salvo'})
 
